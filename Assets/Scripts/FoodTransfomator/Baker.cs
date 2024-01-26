dfe2396a438b3545b8a4b83a8ff0a236f2a7b969
Sasha <132772266+notpatern@users.noreply.github.com>
2024-01-26 16:17:01 +0100

    feat: +FoodTransformator +Fryer +Baker +Chopper

---
+++ Assets/Scripts/FoodTransfomator/Baker.cs
@@ -0,0 +1,14 @@
+public class Baker : FoodTransformator
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
