fe815e5e3d8884298918f406d9c062df33b3bd56
La Loutre Cosmique <sushi.laque77@gmail.com>
2024-02-16 18:10:15 +0100

    feat: base food system

--- Assets/Scripts/FoodSystem/Foods/SimpleFood.cs
+++ Assets/Scripts/FoodSystem/Foods/SimpleFood.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SimpleFood : MonoBehaviour
+public class SimpleFood : Food
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    FoodData _data;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public override string GetContext() => _data.foodName;
 }
