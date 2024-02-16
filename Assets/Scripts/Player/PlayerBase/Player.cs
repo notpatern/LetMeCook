0e2fdd9cd403cb1e9ade8bf05214ac6327c84eeb
Maximilien <maxodie200@gmail.com>
2024-02-16 16:38:41 +0100

    add playerInteraction event + raycast detection

--- Assets/Scripts/Player/PlayerBase/Player.cs
+++ Assets/Scripts/Player/PlayerBase/Player.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] PlayerInteraction playerInteraction;
+    [SerializeField] GameObject playerPrefab;
+
+    private void Start()
     {
-        
+        playerInteraction.BindPerformInteraction(LoadHand);
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadHand(GameObject food, int id)
     {
-        
+        Debug.Log("J'équipe :" + food + " id : " + id);
     }
 }
--- Assets/Scripts/Player/PlayerInteraction/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction/PlayerInteraction.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class PlayerInteraction : MonoBehaviour
+[Serializable]
+public class PlayerInteraction
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    IInteractable m_CurrentInteraction;
+
+    [SerializeField] Transform m_Origin;
+
+    [SerializeField] float m_InteractionMaxDistance;
+    [SerializeField] LayerMask m_LayerMask;
+
+    public UnityEvent<GameObject, int> m_OnInteract;
+
+    public void BindPerformInteraction(UnityAction<GameObject, int> action)
+    { 
+        m_OnInteract.AddListener(action);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update(float dt)
     {
-        
+        TestInteraction();
+    }
+
+    public void TestInteraction()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(m_Origin.position, m_Origin.forward, out hit, m_InteractionMaxDistance, m_LayerMask))
+        {
+            m_CurrentInteraction = hit.transform.GetComponent<IInteractable>();
+
+            if(m_CurrentInteraction)
+            {
+                m_OnInteract.Invoke(m_CurrentInteraction.gameObject, 0);
+            }
+        }
     }
 }
