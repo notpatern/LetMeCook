ad23542d53a182bf47f9243ae370cd53d768883b
Porolito <pierre.thomas16380@gmail.com>
2024-02-26 15:53:22 +0100

    feat: ui-manager +pause menu logic and quit button

--- Assets/Scripts/UI/MENUScripts/PauseMenuUI.cs
+++ Assets/Scripts/UI/MENUScripts/PauseMenuUI.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuUI : MonoBehaviour
 {
-    void SetActiveMenuState(bool state)
+    void ToggleActiveMenuState()
     {
+        bool state = !gameObject.activeSelf;
         gameObject.SetActive(state);
         Cursor.lockState = state ? CursorLockMode.None : CursorLockMode.Locked;
         Cursor.visible = state;
+    }
+
+    void QuitButton(string sceneName)
+    {
+        EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+        sceneName.ToLower();
+        foreach (EditorBuildSettingsScene scene in scenes)
+        {
+            string currentSceneName = scene.path;
+            currentSceneName = currentSceneName.Substring(currentSceneName.LastIndexOf('\\') + 1);
+            currentSceneName = currentSceneName.Substring(0, currentSceneName.Length - 6);
+            if (sceneName == currentSceneName)
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
     }
 }
