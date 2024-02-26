593585ff0cfac27d389f96d0798e39adc5d35dcd
Sasha <132772266+notpatern@users.noreply.github.com>
2024-02-26 16:08:20 +0100

    feat: created folder for movement data

---
+++ Assets/Scripts/PlayerSystems/MovementFSMCore/DataClass/FsmData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PlayerSystems.MovementFSMCore.DataClass
+{
+    public class FsmData : ScriptableObject
+    {
+        public float movementSpeed;
+        public float drag;
+        public float movementMultiplier;
+        public float jumpForce;
+    }
+}
