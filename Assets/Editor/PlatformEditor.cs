e3089b01d7afc4999f338470677c31bee5f647a2
La Loutre Cosmique <sushi.laque77@gmail.com>
2024-02-28 23:10:30 +0100

    feat: editor tests

--- Assets/Editor/PlatformEditor.cs
+++ Assets/Editor/PlatformEditor.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using Level.LevelDesign;
 using UnityEngine;
+using UnityEditor;
 
-public class PlatformEditor : MonoBehaviour
+[CustomEditor(typeof(MovingPlatform))]
+[CanEditMultipleObjects]
+public class LookAtPointEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    SerializedProperty lookAtPoint;
+
+    void OnEnable()
     {
-        
+        lookAtPoint = serializedObject.FindProperty("lookAtPoint");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(lookAtPoint);
+        if (lookAtPoint.vector3Value.y > ((MovingPlatform)target).transform.position.y)
+        {
+            EditorGUILayout.LabelField("(Above this object)");
+        }
+        if (lookAtPoint.vector3Value.y < ((MovingPlatform)target).transform.position.y)
+        {
+            EditorGUILayout.LabelField("(Below this object)");
+        }
         
+            
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public void OnSceneGUI()
+    {
+        var t = (target as MovingPlatform);
+
+        EditorGUI.BeginChangeCheck();
+        Vector3 pos = Handles.PositionHandle(t.lookAtPoint, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Move point");
+            t.lookAtPoint = pos;
+            t.Update();
+        }
     }
 }
--- Assets/Scripts/Level/LevelDesign/MovingPlatform.cs
+++ Assets/Scripts/Level/LevelDesign/MovingPlatform.cs
@@ -4,6 +4,11 @@ namespace Level.LevelDesign
 {
     public class MovingPlatform : MonoBehaviour
     {
-        
+        public Vector3 lookAtPoint = Vector3.zero;
+
+        public void Update()
+        {
+            transform.LookAt(lookAtPoint);
+        }
     }
 }
