461a814bee449499b01bf8d1b11688932af085fc
Sasha <132772266+notpatern@users.noreply.github.com>
2024-04-05 20:08:38 +0200

    fix: changed ConsumeStamins => now returns void; added bool CanConsumeStamina in IStamina

--- Assets/Scripts/PlayerSystems/MovementFSMCore/IStamina.cs
+++ Assets/Scripts/PlayerSystems/MovementFSMCore/IStamina.cs
@@ -3,7 +3,8 @@ namespace PlayerSystems.MovementFSMCore
     public interface IStamina
     {
         public float Stamina { get; set; }
-        public bool ConsumeStamina(float staminaToRemove);
+        public bool CanConsumeStamina(float staminaToRemove);
+        public void ConsumeStamina(float staminaToRemove);
         public void RegenerateStamina(float staminaToRegenerate);
     }
 }
