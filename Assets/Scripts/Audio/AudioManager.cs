5b24c4a5ee841dc81b09242be8fa548986bf7289
Maximilien <maxodie200@gmail.com>
2024-04-09 01:46:57 +0200

    feat : audioManager instance management

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
+using System.Collections.Generic;
 
 namespace Audio
 {
     public class AudioManager
     {
+        List<EventInstance> m_EventInstances;
         public static AudioManager s_Instance { get; private set; }
         public AudioSoundData m_AudioSoundData;
 
@@ -13,6 +16,7 @@ namespace Audio
             if(s_Instance == null)
             {
                 s_Instance = new AudioManager();
+                s_Instance.m_EventInstances = new List<EventInstance>();
             }
 
             s_Instance.m_AudioSoundData = audioSoundData;
@@ -21,6 +25,22 @@ namespace Audio
         public void PlayOneShot(EventReference sound, Vector3 worldPosition)
         {
             RuntimeManager.PlayOneShot(sound, worldPosition);
+        }
+
+        EventInstance CreateInstance(EventReference eventReference)
+        {
+            EventInstance instance = RuntimeManager.CreateInstance(eventReference);
+            m_EventInstances.Add(instance);
+            return instance;
+        }
+
+        void CleanUp()
+        {
+            foreach(EventInstance instance in m_EventInstances)
+            {
+                instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                instance.release();
+            }
         }
     }
 
