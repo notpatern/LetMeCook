11fa08372da62e5a799e9005d5cd8d93f9cba0f4
Sasha <132772266+notpatern@users.noreply.github.com>
2024-04-15 01:24:35 +0200

    feat: following eyes

--- Assets/Scripts/LevelGameplayObject/WallrunPlatform.cs
+++ Assets/Scripts/LevelGameplayObject/WallrunPlatform.cs
@@ -1,14 +1,27 @@
+
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class WallrunPlatform : MonoBehaviour
 {
     [SerializeField] GameEventScriptableObject m_GameEvent;
     [SerializeField] GameObject m_eyeLid;
+    [SerializeField] GameEventScriptableObject m_playerPos;
+    private Transform m_playerTransform;
+    [SerializeField] GameObject m_leftPupil;
+    [SerializeField] GameObject m_rightPupil;
+    private Quaternion leftRotation;
+    private Quaternion rightRotation;
 
     void Awake()
     {
+        m_playerPos.BindEventAction((object playerTransform) =>
+        {
+            m_playerTransform = (Transform)playerTransform;
+        });
         m_eyeLid.SetActive(true);
+        
+        leftRotation = m_leftPupil.transform.rotation;
+        rightRotation = m_rightPupil.transform.rotation;
     }
 
     void OnEnable()
@@ -24,5 +37,24 @@ public class WallrunPlatform : MonoBehaviour
     void ActiveLight(object args)
     {
         m_eyeLid.SetActive(!(bool)args);
+    }
+
+    private void Update()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        Vector3 position = m_playerTransform.position;
+        
+        Vector3 leftPupilDirection = position - m_leftPupil.transform.position;
+        Vector3 rightPupilDirection = position - m_rightPupil.transform.position;
+
+        Quaternion targetLeftRotation = Quaternion.LookRotation(leftPupilDirection);
+        Quaternion targetRightRotation = Quaternion.LookRotation(rightPupilDirection);
+
+        m_leftPupil.transform.rotation = Quaternion.RotateTowards(leftRotation, targetLeftRotation, 16.2256f);
+        m_rightPupil.transform.rotation = Quaternion.RotateTowards(rightRotation, targetRightRotation, 16.2256f);
     }
 }
