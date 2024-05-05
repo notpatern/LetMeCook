764d14dbece4b818b66b3fecb4ed9078871c92c2
Maximilien <maxodie200@gmail.com>
2024-05-05 18:06:41 +0200

    feat : fmod sounds setup +project rename + splash image

--- Assets/Scripts/Audio/AudioSoundData.cs
+++ Assets/Scripts/Audio/AudioSoundData.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="AudioSoundData", menuName ="LetMeCook/Audio/AudioSoundData")]
 public class AudioSoundData : ScriptableObject
 {
-    [Header("Player")]
+    [Header("PlayerFoodAction")]
     public EventReference m_PlayerThrowSound;
+    public EventReference m_PlayerPickFood;
+    public EventReference m_PlayerMergeFood;
+    public EventReference m_PlayerCrunchFood;
+
+    [Header("PlayerMovement")]
+    public EventReference m_PlayerGroundImpact;
+    public EventReference m_PlayerJump;
+    public EventReference m_PlayerWallrun;
+    public EventReference m_PlayerDoubleJump;
+    public EventReference m_PlayerDash;
+
+    [Header("Food")]
+    public EventReference m_FoodBounceImpact;
+    public EventReference m_FoodAirThrowingEffect;
+
+    [Header("Transformator")]
+    public EventReference m_BakerCooking;
+    public EventReference m_ChopperCooking;
+    public EventReference m_PurifierCooking;
+
+    [Header("MouthGoal")]
+    public EventReference m_DeliverySound;
+
 }
