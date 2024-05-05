c710c6aeca2a9d8ef9e706f9425d803a1a076f07
Maximilien <maxodie200@gmail.com>
2024-05-06 00:12:27 +0200

    fix : mouth stay

--- Assets/Scripts/Recipes/RecipeGoal.cs
+++ Assets/Scripts/Recipes/RecipeGoal.cs
@@ -33,6 +33,11 @@ namespace RecipeSystem.Core
             }
         }
 
+        void OnTriggerStay(Collider other)
+        {
+            OnTriggerEnter(other);
+        }
+
         protected virtual void OnFoodOk(int potentialRecipe)
         {
             postProcessingManager.ChangeChromaticAberration(1f, 2.5f);
