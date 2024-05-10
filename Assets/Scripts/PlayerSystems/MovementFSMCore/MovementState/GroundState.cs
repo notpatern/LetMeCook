923a18babf980baae0620a993e013bc87af4579e
Maximilien <maxodie200@gmail.com>
2024-05-10 18:33:42 +0200

    fix : pull commit

--- Assets/Scripts/PlayerSystems/MovementFSMCore/MovementState/GroundState.cs
+++ Assets/Scripts/PlayerSystems/MovementFSMCore/MovementState/GroundState.cs
@@ -6,8 +6,35 @@ namespace PlayerSystems.MovementFSMCore.MovementState
 {
     public class GroundState : FsmState
     {
+        private readonly GroundContext _context;
+
         public GroundState(GroundContext context, MovementFsmCore fsmCore) : base(context, fsmCore)
         {
+            _context = context;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            FrictionManager();
+        }
+
+        private void FrictionManager()
+        {
+            if (fsmCore.jumpHeld)
+            {
+                fsmCore.rb.drag = 0f;
+                return;
+            }
+
+            if (fsmCore.Input == new Vector2 (0, 0))
+            {
+                fsmCore.rb.drag = _context.friction;
+            }
+            else
+            {
+                fsmCore.rb.drag = context.drag;
+            }
         }
 
         public override void Jump()
