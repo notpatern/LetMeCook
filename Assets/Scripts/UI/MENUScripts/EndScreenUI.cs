8b28fb7c0866d3efbeff94acf296263c233d296e
Maximilien <40921999+Maxodie@users.noreply.github.com>
2024-06-04 11:11:20 +0200

    remove : debug

--- Assets/Scripts/UI/MENUScripts/EndScreenUI.cs
+++ Assets/Scripts/UI/MENUScripts/EndScreenUI.cs
@@ -66,7 +66,6 @@ public class EndScreenUI : MonoBehaviour
 
         if (!nextLevelData)
         {
-            Debug.Log("te");
             Destroy(m_NextLevelButton.gameObject);
         }
         if (playerScore.m_RequiredScore > playerScore.m_Score && data.m_LevelReached <= levelData.levelID)
