1a78992ddacb92f0859ee16e6ca9cfdfd2961099
La Loutre Cosmique <sushi.laque77@gmail.com>
2024-06-19 19:00:20 +0200

    feat: tuto part5

--- Assets/Scripts/LevelGameplayObject/Tutorial/TutorialDropperSetPlayerInputsActive.cs
+++ Assets/Scripts/LevelGameplayObject/Tutorial/TutorialDropperSetPlayerInputsActive.cs
@@ -6,13 +6,16 @@ public class TutorialDropperSetPlayerInputsActive : FoodDropper
 {
     [Header("Tutorial properties")]
     [SerializeField] GameEventScriptableObject m_PlayerSetActiveInput;
-    [SerializeField] KeybindsData m_KeybindsData;
+    [SerializeField] KeybindsData m_KeybindsDataLeft;
+    [SerializeField] KeybindsData m_KeybindsDataRight;
     [SerializeField] bool m_State;
 
     public override GameObject StartInteraction()
     {
-        Tuple<KeybindsData, bool> action = new (m_KeybindsData, m_State);
-        m_PlayerSetActiveInput.TriggerEvent(action);
+        Tuple<KeybindsData, bool> actionL = new (m_KeybindsDataLeft, m_State);
+        Tuple<KeybindsData, bool> actionR = new (m_KeybindsDataRight, m_State);
+        m_PlayerSetActiveInput.TriggerEvent(actionL);
+        m_PlayerSetActiveInput.TriggerEvent(actionR);
         
         return base.StartInteraction();
     }
